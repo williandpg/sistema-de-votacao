c7f2e9bbdb06eeb3ced6000c90c82479a942da69
Willian Gonçalves <willian.p.g@outlook.com>
2024-03-07 16:37:24 -0300

    Requisito 03 - Implemente a classe PessoaEleitora

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,8 +1,22 @@
 package com.betrybe.sistemadevotacao;
 
 /**
- * Check.
+ * Req 03 - Implemente a classe PessoaEleitora.
  */
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public PessoaEleitora(String nome, String cpf) {
+    this.setNome(nome);
+    this.cpf = cpf;
+  }
 
 }
