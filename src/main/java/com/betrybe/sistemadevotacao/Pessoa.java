7d4506ba479d43354b7a2f544f3b82320baafdb6
Willian Gonçalves <willian.p.g@outlook.com>
2024-03-07 15:52:30 -0300

    Requisito 01 - Implemente a classe abstrata Pessoa

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,17 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-  
+/**
+ * Req 01 - Implemente a classe abstrata Pessoa.
+ */
+public abstract class Pessoa {
+  protected String nome;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
 }
