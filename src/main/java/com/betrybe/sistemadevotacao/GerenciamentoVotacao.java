7632da49d648075a2996b09863f0e964bcc74db2
Willian Gonçalves <willian.p.g@outlook.com>
2024-03-08 01:30:20 -0300

    Requisito 06 - Implementar os métodos de votação da classe GerenciamentoVotacao

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 /**
- * Req 04 - Implementar a classe GerenciamentoVotacao com atributos.
+ * Req 04, 05 e 06 - Implementar a classe GerenciamentoVotacao com atributos,
+ * os métodos de cadastro da classe GerenciamentoVotacao
+ * e os métodos de votação da classe GerenciamentoVotacao.
  */
 public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
   ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<>();
@@ -33,12 +35,29 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
   @Override
   public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
-
+    if (cpfsComputados.contains(cpfPessoaEleitora)) {
+      System.out.println("Pessoa eleitora já votou!");
+    }
+    for (PessoaCandidata pessoaCandidata : pessoasCandidatas) {
+      if (Objects.equals(pessoaCandidata.getNumero(), numeroPessoaCandidata)) {
+        pessoaCandidata.receberVoto();
+        cpfsComputados.add(cpfPessoaEleitora);
+      }
+    }
   }
 
   @Override
   public void mostrarResultado() {
-
+    if (cpfsComputados.isEmpty()) {
+      System.out.println("É preciso ter pelo menos um voto para mostrar o resultado.");
+    }
+    for (PessoaCandidata pessoaCandidata : pessoasCandidatas) {
+      String nome = pessoaCandidata.getNome();
+      int votos = pessoaCandidata.getVotos();
+      int votosTotal = cpfsComputados.size();
+      int votosPercentual = Math.round(((float) votos / votosTotal) * 100);
+      System.out.printf("Nome: %s - %d votos ( %s )%n", nome, votos, votosPercentual);
+      System.out.printf("Total de votos: %s%n", votosTotal);
+    }
   }
-
 }
