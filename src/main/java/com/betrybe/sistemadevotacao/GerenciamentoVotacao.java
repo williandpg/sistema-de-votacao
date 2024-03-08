a40719db0722c20217d5e01ad5727de6f5f9fdca
Willian Gonçalves <willian.p.g@outlook.com>
2024-03-08 00:51:49 -0300

    Requisitos 04 e 05 - Implementar a classe GerenciamentoVotacao com atributos e os métodos de cadastro da classe GerenciamentoVotacao

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,8 +1,44 @@
 package com.betrybe.sistemadevotacao;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 /**
- * Check.
+ * Req 04 - Implementar a classe GerenciamentoVotacao com atributos.
  */
-public class GerenciamentoVotacao {
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+  ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<>();
+  ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<>();
+  ArrayList<String> cpfsComputados = new ArrayList<>();
+
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    for (PessoaCandidata pessoaCandidata : pessoasCandidatas) {
+      if (Objects.equals(pessoaCandidata.getNumero(), numero)) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+      }
+    }
+    pessoasCandidatas.add(new PessoaCandidata(nome, numero));
+  }
+
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    for (PessoaEleitora pessoaEleitora : pessoasEleitoras) {
+      if (Objects.equals(pessoaEleitora.getCpf(), cpf)) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+      }
+    }
+    pessoasEleitoras.add(new PessoaEleitora(nome, cpf));
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  @Override
+  public void mostrarResultado() {
+
+  }
 
 }
